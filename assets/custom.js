92e043331f21f9e08dc9b96a3b5b4a9646e365de
Dennis Zheng <dennisszheng@gmail.com>
2023-10-29 10:18:26 -0700

    Updates - Hid image on small resolution screen size - Adjusted to have projects have links on hover for image

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,9 @@
 // Add custom JS
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.nav-toggle');
+
+  navToggle.addEventListener('click', function() {
+      navbar.classList.toggle('active'); // Toggle the 'active' class on the navbar
+  });
+});
